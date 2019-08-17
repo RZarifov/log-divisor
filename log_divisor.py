b5bfcd35430fbcdbd7fa61a069c60a2a4ed5e376
Ruslan Zarifov <xeizzen@gmail.com>
2019-08-17 22:46:58 +0300

    properly handle corrupted log files

--- log_divisor.py
+++ log_divisor.py
@@ -12,22 +12,23 @@ logger = getLogger(__name__)
 class LogDivisor(object):
     date_re = re.compile(r'(\d+-\d+-\d+ \d+:\d+:\d+)')
 
-    def __init__(self, log_file_name, 
+    def __init__(self, log_file_path, 
             save_folder_path = None,
             date_format_re = None):
         if date_format_re:
             self.date_re = date_format_re
 
-        self.log_file_name = log_file_name
+        self.log_file_path = log_file_path
+        self.filename, _ = os.path.splitext(log_file_path)
         self.log_subfiles = defaultdict(str)
         self.save_folder_path = save_folder_path
 
         try:
-            self.log_file = open(self.log_file_name, "r")
+            self.log_file = open(self.log_file_path, "r")
         except OSError as exception:
             log_message = textwrap.dedent(f"""\
                 The program experienced an error: {exception.strerror},
-                While trying to open the file \"{self.log_file_name}\".
+                While trying to open the file \"{self.log_file_path}\".
                 {"="*80}""")
 
             logger.error(log_message, exc_info=True)
@@ -40,29 +41,36 @@ class LogDivisor(object):
         match = self.date_re.search(line)
         if match:
             matchDate = match.group(1)
-            dt = datetime.strptime(matchDate, '%Y-%m-%d %H:%M:%S')
+            try:
+                dt = datetime.strptime(matchDate, '%Y-%m-%d %H:%M:%S')
+            except ValueError:
+                return self._get_corrupt_entries_file_path()
 
             if yearly:
                 base_name = f"{dt.year}"
             else:
                 base_name = f"{dt.year}/{dt.strftime('%b')}"
 
-            filename, _ = os.path.splitext(self.log_file.name)
             if self.save_folder_path:
                 subfile_name = f"{self.provided_path}/{dt.year}.log"
             else:
-                if os.path.isfile(filename):
-                    subfile_name = f"{filename}_divided/{base_name}.log"
+                if os.path.isfile(self.filename):
+                    subfile_name = f"{self.filename}_divided/{base_name}.log"
                 else:
-                    subfile_name = f"{filename}/{base_name}.log"
+                    subfile_name = f"{self.filename}/{base_name}.log"
+            return subfile_name
         else:
-            if self.save_folder_path:
-                subfile_name = f"{self.provided_path}/corrupt_log_entries.log"
+            return self._get_corrupt_entries_file_path()
+
+    def _get_corrupt_entries_file_path(self):
+        if self.save_folder_path:
+            subfile_name = f"{self.provided_path}/corrupt_log_entries.log"
+        else:
+            if os.path.isfile(self.filename):
+                subfile_name = f"{self.filename}_divided/corrupt_log_entries.log"
             else:
-                if os.path.isfile(filename):
-                    subfile_name = f"{filename}_divided/corrupt_log_entries.log"
-                else:
-                    subfile_name = f"{filename}/corrupt_log_entries.log"
+                subfile_name = f"{self.filename}/corrupt_log_entries.log"
+        
         return subfile_name
 
     def _check_directory(_, subfile_name):
@@ -81,7 +89,7 @@ class LogDivisor(object):
     def divide_month_wise(self):
         self._divide_file(yearly = False)
 
-    def _divide_file(self, yearly = True):
+    def _divide_file(self, yearly):
         for line in self.log_file:
             subfile_name = self._get_subfile_name(line, yearly)
 
@@ -98,11 +106,6 @@ class LogDivisor(object):
                 self.log_subfiles.pop(key)
 
 
-def get_new_log_subfile(name, year=None, month=None):
-    if year:
-        return open(f'{name}/{year}')
-
-
 if __name__ == '__main__':
-    ld = LogDivisor('test_files/sample_large.log')
-    ld.divide_log_file()
+    ld = LogDivisor('test_files/corrupt.log')
+    ld.divide_month_wise()
